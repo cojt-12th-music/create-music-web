98a17e6e805f74eeae2437d0daf93ffd486f7a57
SIY1121 <sota.0512.0213@gmail.com>
2020-05-29 22:58:34 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>気軽に音楽を創りたい！</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer fixed app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
