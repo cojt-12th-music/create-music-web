088945eabaa59ee8e6fd7d00768ba0157add7dbb
takonasu <piano88_viola_tako8@hotmail.co.jp>
2020-06-05 16:46:46 +0900

    add: ブロック操作

--- components/musicalScore.vue
+++ components/musicalScore.vue
@@ -1,16 +1,75 @@
 <template>
-  <div id="component-frame" class="red lighten-2">
-    <p>musicalScoreComponent</p>
+  <div id="component-frame">
+    <v-container>
+      <v-row>
+        <v-col cols="4">
+          <ul id="rhythm" class="blue lighten-3">
+            <li>リズム</li>
+            <li>
+              <draggable element="ul" class="draggable">
+                <li v-for="block in rhythmBlocks" :key="block">{{ block }}</li>
+              </draggable>
+            </li>
+          </ul>
+        </v-col>
+        <v-col cols="4">
+          <ul id="code" class="green lighten-3">
+            <li>コード</li>
+            <li>
+              <draggable element="ul" class="draggable">
+                <li v-for="block in codeBlocks" :key="block">{{ block }}</li>
+              </draggable>
+            </li>
+          </ul>
+        </v-col>
+        <v-col cols="4">
+          <ul id="melody" class="pink lighten-3">
+            <li>メロディ</li>
+            <li>
+              <draggable element="ul" class="draggable">
+                <li v-for="block in melodyBlocks" :key="block">{{ block }}</li>
+              </draggable>
+            </li>
+          </ul>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
+import draggable from 'vuedraggable'
+export default {
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      rhythmBlocks: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+      codeBlocks: ['C', 'D', 'E', 'F', 'G', 'A', 'B'],
+      melodyBlocks: ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 div#component-frame {
   height: 100%;
+  overflow: scroll;
+}
+
+ul {
+  list-style: none;
+  text-align: center;
+  padding: 0;
+}
+ul.draggable {
+  li {
+    margin: 1rem;
+    display: block;
+    background: #c0c0c0;
+    height: 5rem;
+  }
 }
 </style>
