e2dec33315902cd9fae1d64113e63c6bc1cf23a8
SIY1121 <sota.0512.0213@gmail.com>
2020-06-05 22:54:27 +0900

    update: load mp3

--- components/Instrument.vue
+++ components/Instrument.vue
@@ -90,29 +90,18 @@ export default Vue.extend({
       await fetch(this.encodedSfzPath)
         .then((res) => res.json())
         .then((sfz) => {
-          this.sampleDefinition = sfz
+          this.sampleDefinition = sfz.sfz
+          return Promise.all(
+            Object.keys(sfz.samples).map((key) =>
+              this.context
+                .decodeAudioData(this.base64ToArrayBuffer(sfz.samples[key]))
+                .then((buf) => {
+                  this.samples[key] = buf
+                })
+            )
+          )
         })
-
-      // 楽器の音源データーを取得
-      await Promise.all(
-        this.sampleDefinition.map((d) =>
-          fetch(`${this.encodedSfzParentPath}/${this.encodePath(d.sample)}`)
-            .then((res) => res.arrayBuffer()) // arrayBuffer（バイナリデータ）として受け取る
-            .then((arr) => this.context.decodeAudioData(arr)) // WebAudioAPIで再生できる形式(AudioBuffer)へ変換
-            .then((buf) => {
-              this.samples[d.sample] = buf
-              /**
-               * samplesにはマップのような形で音のデータを格納
-               * {
-               *  'samples/C4.wav': [音のデータ],
-               *  'samples/D4.wav': [音のデータ],
-               *  'samples/E4.wav': [音のデータ]
-               * }
-               */
-            })
-        )
-      )
-      this.isLoading = false
+        .then(() => (this.isLoading = false))
     },
     /**
      * 音を鳴らす
@@ -197,27 +186,69 @@ export default Vue.extend({
     parentDir(path: string) {
       const tmp = path.split('/')
       return tmp.slice(0, tmp.length - 1).join('/')
+    },
+    base64ToArrayBuffer(base64: string): ArrayBuffer {
+      const Base64Binary = {
+        _keyStr:
+          'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+
+        /* will return a  Uint8Array type */
+        decodeArrayBuffer(input: string) {
+          const bytes = (input.length / 4) * 3
+          const ab = new ArrayBuffer(bytes)
+          this.decode(input, ab)
+
+          return ab
+        },
+
+        removePaddingChars(input: string) {
+          const lkey = this._keyStr.indexOf(input.charAt(input.length - 1))
+          // eslint-disable-next-line eqeqeq
+          if (lkey == 64) {
+            return input.substring(0, input.length - 1)
+          }
+          return input
+        },
+
+        decode(input: string, arrayBuffer: ArrayBuffer) {
+          input = this.removePaddingChars(input)
+          input = this.removePaddingChars(input)
+          // @ts-ignore
+          const bytes = parseInt((input.length / 4) * 3, 10)
+
+          let uarray
+          let chr1, chr2, chr3
+          let enc1, enc2, enc3, enc4
+          let i = 0
+          let j = 0
+
+          if (arrayBuffer) uarray = new Uint8Array(arrayBuffer)
+          else uarray = new Uint8Array(bytes)
+
+          input = input.replace(/[^A-Za-z0-9+/=]/g, '')
+
+          for (i = 0; i < bytes; i += 3) {
+            enc1 = this._keyStr.indexOf(input.charAt(j++))
+            enc2 = this._keyStr.indexOf(input.charAt(j++))
+            enc3 = this._keyStr.indexOf(input.charAt(j++))
+            enc4 = this._keyStr.indexOf(input.charAt(j++))
+
+            chr1 = (enc1 << 2) | (enc2 >> 4)
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+            chr3 = ((enc3 & 3) << 6) | enc4
+
+            uarray[i] = chr1
+            // eslint-disable-next-line eqeqeq
+            if (enc3 != 64) uarray[i + 1] = chr2
+            // eslint-disable-next-line eqeqeq
+            if (enc4 != 64) uarray[i + 2] = chr3
+          }
+
+          return uarray
+        }
+      }
+      return Base64Binary.decodeArrayBuffer(base64)
     }
   }
 })
 </script>
-
-<style lang="scss" scoped>
-.playground {
-  padding: 1rem;
-  background: #eee;
-}
-.playground:focus {
-  background: #f98000;
-}
-.playground:focus::after {
-  content: '入力受付中';
-}
-.logarea {
-  height: 150px;
-  overflow-y: scroll;
-  .log {
-    text-align: left;
-  }
-}
-</style>
