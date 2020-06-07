193975c4fa700790222f994437dbf12b0188cc65
Kazuya Izumi <shikibu9419@gmail.com>
2020-06-07 22:01:51 +0900

    add: actions to music store

--- store/music.ts
+++ store/music.ts
@@ -29,28 +29,51 @@ export type MusicState = ReturnType<typeof state>
 export const getters = getterTree(state, {
   // メロディーのblocksを返すgetter
   melodyBlocks: (state: MusicState): Block[] =>
-    state.melody.blockLabels.map((label) => state.blocks.melody[label] || {})
+    state.melody.blockLabels.map((label) => state.blocks.melody[label])
 })
 
 export const mutations = mutationTree(state, {
+  // blockを新しく作る
+  ADD_BLOCK(state: MusicState, block: Block) {
+    state.blocks.melody[block.label] = block
+  },
   // メロディーの特定のブロックに新しいsoundを追加する
-  pushSoundToMelody(
+  ADD_SOUND(
     state: MusicState,
     { blockLabel, sound }: { blockLabel: string; sound: Sound }
   ) {
+    sound.id = state.blocks.melody[blockLabel].sounds.length + 1
     state.blocks.melody[blockLabel].sounds.push(sound)
+  },
+  // soundを変更する
+  UPDATE_SOUND(
+    state: MusicState,
+    { blockLabel, sound }: { blockLabel: string; sound: Sound }
+  ) {
+    state.blocks.melody[blockLabel].sounds.splice((sound.id || 0) - 1, 1, sound)
   }
 })
 
 export const actions = actionTree(
   { state, getters, mutations },
   {
+    // blockを新しく作るaction
+    addBlock({ commit }, block: Block) {
+      commit('ADD_BLOCK', block)
+    },
     // soundを追加するaction
-    pushSound(
+    addSound(
       { commit },
       { blockLabel, sound }: { blockLabel: string; sound: Sound }
     ) {
-      commit('pushSoundToMelody', { blockLabel, sound })
+      commit('ADD_SOUND', { blockLabel, sound })
+    },
+    // soundを追加するaction
+    updateSound(
+      { commit },
+      { blockLabel, sound }: { blockLabel: string; sound: Sound }
+    ) {
+      commit('UPDATE_SOUND', { blockLabel, sound })
     }
   }
 )
