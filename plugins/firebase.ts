2e4f99f8e8ce264fc18672847174c3689b2997e2
Kazuya Izumi <shikibu9419@gmail.com>
2020-06-25 23:55:36 +0900

    add: firestore settings

---
+++ plugins/firebase.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyAaEDTy_mldrO6YQzjZ5435Yx9IkMx-Qxk',
+    authDomain: 'create-music-web.firebaseapp.com',
+    databaseURL: 'https://create-music-web.firebaseio.com',
+    messagingSenderId: '463511103386',
+    projectId: 'create-music-web',
+    storageBucket: 'create-music-web.appspot.com'
+  })
+}
+
+export default firebase
