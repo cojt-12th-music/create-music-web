deac1cc4d8097a1d3cd350156bd4221293161759
Kazuya Izumi <shikibu9419@gmail.com>
2020-07-03 18:21:38 +0900

    update: re-rename component

---
+++ components/Lesson.vue
@@ -0,0 +1,66 @@
+<template><div /></template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Element } from '@/types/lesson'
+
+export default Vue.extend({
+  beforeMount() {
+    Object.assign(this.$driver.options, {
+      allowClose: false,
+      opacity: 0,
+      nextBtnText: 'OK!',
+      prevBtnText: '戻る',
+      closeBtnText: 'やめる',
+      doneBtnText: '終わる',
+      showButtons: true,
+      onNext: (step: Element) => {
+        if (step && step.options.delay) {
+          const currentIndex = this.$driver.currentStep
+          this.$driver.preventMove()
+          this.$driver.reset()
+          setTimeout(() => {
+            this.setSteps()
+            this.$driver.start(currentIndex + 1)
+          }, step.options.delay)
+        }
+      },
+      onHighlightStarted: (step: Element) => {
+        const nextEvent = step.options.nextEvent
+        if (nextEvent) {
+          const elem = document.getElementById(nextEvent.element.slice(1))
+          if (!elem) {
+            return
+          }
+          const f = () => {
+            this.$driver.handleNext()
+            elem.removeEventListener('click', f, false)
+          }
+          elem.addEventListener('click', f, false)
+        }
+      },
+      onHighlighted: (step: Element) => {
+        if (step.options.disableTouch) {
+          step.node.classList.remove('driver-highlighted-element')
+        }
+      }
+    })
+  },
+  mounted() {
+    if (this.$accessor.lesson.isAvailable) {
+      this.lessonStart()
+    }
+  },
+  methods: {
+    lessonStart() {
+      this.setSteps()
+      this.$driver.start()
+    },
+    setSteps() {
+      this.$driver.defineSteps(
+        this.$accessor.lesson.steps.map((step, index) => ({ ...step, index }))
+      )
+    }
+  }
+})
+</script>
