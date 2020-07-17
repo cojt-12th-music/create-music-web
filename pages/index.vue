310d5fcc39fd2b045371dd22f27358071d019adb
Kazuya Izumi <shikibu9419@gmail.com>
2020-07-17 16:18:43 +0900

    update: move block-trash component location

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,15 @@
 <template>
   <div id="component-frame">
     <div id="musical-score-wrapper">
-      <musical-score />
+      <musical-score :trash-part.sync="trashPart" />
     </div>
+
+    <transition name="trash">
+      <div v-if="!isPlaying && trashPart" class="score-trash-wrapper">
+        <draggable class="score-draggable-trash" :group="trashPart" />
+        <v-icon class="icon">fa-trash</v-icon>
+      </div>
+    </transition>
 
     <div id="operation-area-wrapper">
       <operation-area />
@@ -12,20 +19,24 @@
 </template>
 
 <script lang="ts">
+import Vue from 'vue'
 import { Context } from '@nuxt/types'
+import draggable from 'vuedraggable'
 
 import MusicalScore from '@/components/musicalScore.vue'
 import OperationArea from '@/components/operationArea.vue'
 import Player from '@/components/Player.vue'
 import { firebaseAuth, firestoreAccessor } from '@/plugins/firebase'
-import { Music } from '@/types/music'
+import { Music, ScorePart } from '@/types/music'
 
 type DataType = {
   dialog: boolean
+  trashPart: ScorePart | null
 }
 
-export default {
+export default Vue.extend({
   components: {
+    draggable,
     MusicalScore,
     OperationArea,
     Player
@@ -63,13 +74,16 @@ export default {
   },
   data(): DataType {
     return {
-      dialog: false
+      dialog: false,
+      trashPart: null
     }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
+$operation-area-height: 10vh;
+
 #component-frame {
   background-color: $-gray-800;
   height: 100vh;
@@ -96,10 +110,48 @@ export default {
 }
 
 #operation-area-wrapper {
-  height: 10vh;
+  height: $operation-area-height;
   width: 100vw;
   position: fixed;
   bottom: 0;
+}
+
+.score-trash-wrapper {
+  position: absolute;
+  left: 0;
+  bottom: $operation-area-height;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 5rem;
+  width: 100%;
+
+  .score-draggable-trash {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $-gray-500;
+    border-radius: 50%;
+    height: 4rem;
+    width: 4rem;
+    opacity: 0.5;
+  }
+
+  .icon {
+    position: absolute;
+    color: $-gray-50;
+  }
+}
+
+.trash-enter-active,
+.trash-leave-active {
+  transform: translateY(0) translateZ(0);
+  transition: transform 100ms linear 100ms;
+}
+
+.trash-enter,
+.trash-leave-to {
+  transform: translateY(10vh) translateZ(0);
 }
 
 @include pc {
